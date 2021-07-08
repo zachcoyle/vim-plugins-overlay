b8e1a2c6bb3072aedd65de8b15ddbc39a4fdafa9
Zach Coyle <zach.coyle@gmail.com>
2021-07-08 05:05:55 -0400

    add calvera-dark.nvim

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     barbar-nvim = { url = github:romgrk/barbar.nvim; flake = false; };
     blamer-nvim = { url = github:APZelos/blamer.nvim; flake = false; };
+    calvera-dark-nvim = { url = github:yashguptaz/calvera-dark.nvim; flake = false; };
     focus-vim = { url = github:merlinrebrovic/focus.vim; flake = false; };
     formatter-nvim = { url = github:mhartington/formatter.nvim; flake = false; };
     fzf-lsp-nvim = { url = github:gfanto/fzf-lsp.nvim; flake = false; };
